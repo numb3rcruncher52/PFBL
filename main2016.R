0fda20559f1aec80cdb6bbb27767780dfc274f59
Max <mwlyons52@gmail.com>
2016-01-23 08:57:27 -0500

    Removed unnecessary name matching

--- main2016.R
+++ main2016.R
@@ -24,22 +24,6 @@ batters2 <- batters %>% mutate(wOBA = calcWOBA(SEASON,AB,X1B,X2B,X3B,HR,UBB,HBP,
   mutate(minPA=ifelse(totPA <200,0,0.7*totPA), maxPAvsL=ifelse(totPA >= 502 | LHP_OPS <= .65,160,pmin(LHP_PA*1.33,160)), maxPAvsR=ifelse(totPA >= 502 | RHP_OPS <= .65, 545, pmin(RHP_PA*1.33,545))) %>% 
   mutate(LHP_Runs = maxPAvsL*LHP_wRAA, RHP_Runs = maxPAvsR*RHP_wRAA, Total_Runs = LHP_Runs + RHP_Runs) %>% 
   arrange(-Total_Runs)
-
-## Merge cleaned fielding data
-# Clean names in both data frames for merging
-fielders2 <- fielders %>% separate(Name, c("LastName","extraName"), sep=",") %>% mutate(Team = ifelse(is.na(Team),"FA",Team))
-batters3 <- batters2 %>% separate(Name, c("FirstName","LastName"), sep=" ",extra = "merge") %>% transform(LastName = sub("[^A-Za-z]","",LastName))
-
-# Fix any discrepancies here
-fielders2$LastName[fielders2$LastName=="d'Arnaud"] <- "dArnaud"
-fielders2$LastName[fielders2$LastName=="de Aza"] <- "deAza*"
-fielders2$LastName[fielders2$LastName=="De Jesus"] <- "DeJesus"
-fielders2$LastName[fielders2$LastName=="den Dekker"] <- "denDekker*"
-fielders2$LastName[fielders2$LastName=="Kang"] <- "HoKang"
-fielders2$LastName[fielders2$LastName=="La Stella"] <- "LaStella*"
-fielders2$LastName[fielders2$LastName=="Murphy" & fielders2$Team == "GCJ"] <- "RyanMurphy"
-fielders2$LastName[fielders2$LastName=="O'Malley"] <- "OMalley#"
-fielders2$LastName[fielders2$LastName=="Van Slyke"] <- "VanSlyke"
   
 # Complete merge and calculate runs
 batters_final <- fielders %>% left_join(select(batters2,Name,Team,Age,minPA,maxPAvsL,maxPAvsR,LHP_wOBA,RHP_wOBA, LHP_Runs,RHP_Runs,Total_Runs)) %>% 
@@ -49,7 +33,16 @@ rm(batters,batters2,fielders)
 
 # Calculate runs for pitchers
 names(pitchers)[9:11] <- c("X1B","X2B","X3B")
-pitchers_final <- pitchers %>% mutate(wOBA = calcWOBA(SEASON,AB,X1B,X2B,X3B,HR,UBB,HBP,SF)) %>% select(Name,Team,P,Age,Split,PA,AVG,OBP,SPC,wOBA) %>% gather(stat_name,stat_value,PA:wOBA) %>% mutate(Category = paste(Split,"_",stat_name,sep="")) %>% select(-Split,-stat_name) %>% spread(Category,stat_value) %>% mutate(LHB_wRAA = -1*calcWRAA(LHB_wOBA, LG_WOBA, WOBA_SCALE, 1), RHB_wRAA = -1*calcWRAA(RHB_wOBA, LG_WOBA, WOBA_SCALE, 1),totPA = LHB_PA + RHB_PA) %>% mutate(minPA=ifelse(totPA <200,0,0.7*totPA), maxPAvsL=ifelse(totPA >= 502,160,pmin(LHB_PA*1.25,160)), maxPAvsR=ifelse(totPA >= 502, 545, pmin(RHB_PA*1.25,545))) %>% mutate(LHB_Runs = maxPAvsL*LHB_wRAA, RHB_Runs = maxPAvsR*RHB_wRAA, Total_Runs = LHB_Runs + RHB_Runs) %>% arrange(-Total_Runs)
+pitchers_final <- pitchers %>% filter()  mutate(wOBA = calcWOBA(SEASON,AB,X1B,X2B,X3B,HR,UBB,HBP,SF)) %>% 
+  select(Name,Team,P,Age,Split,PA,Inn,AVG,OBP,SPC,wOBA) %>% 
+  gather(stat_name,stat_value,PA:wOBA) %>% 
+  mutate(Category = paste(Split,"_",stat_name,sep="")) %>% 
+  select(-Split,-stat_name) %>% 
+  spread(Category,stat_value) %>% 
+  mutate(LHB_wRAA = -1*calcWRAA(LHB_wOBA, LG_WOBA, WOBA_SCALE, 1), RHB_wRAA = -1*calcWRAA(RHB_wOBA, LG_WOBA, WOBA_SCALE, 1),totPA = LHB_PA + RHB_PA) %>% 
+  mutate(minPA=ifelse(totPA <200,0,0.7*totPA), maxPAvsL=ifelse(totPA >= 502,160,pmin(LHB_PA*1.25,160)), maxPAvsR=ifelse(totPA >= 502, 545, pmin(RHB_PA*1.25,545))) %>% 
+  mutate(LHB_Runs = maxPAvsL*LHB_wRAA, RHB_Runs = maxPAvsR*RHB_wRAA, Total_Runs = LHB_Runs + RHB_Runs) %>% 
+  arrange(-Total_Runs)
 
 pitchers_final2 <- pitchers_final %>% select(Name, Team, P, Age, LHB_wOBA, RHB_wOBA, totPA, minPA, maxPAvsL, maxPAvsR, LHB_Runs, RHB_Runs, Total_Runs)
 write.csv(pitchers_final2, paste(sheet_path,"pitchers_2016.csv",sep=""),row.names = FALSE)
