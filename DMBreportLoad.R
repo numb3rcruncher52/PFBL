ab06526be211546843cc574f7843856da9d55956
Max Lyons <MaxL@IronHill.local>
2016-07-28 16:31:59 -0400

    Added function to read a DMB Report

---
+++ DMBreportLoad.R
@@ -0,0 +1,35 @@
+### DMBreportLoad.R
+
+## Takes any .txt file report from DMB and returns a clean dataframe
+
+report_path <- "C:\\dmb11\\PFBL 2016\\reports\\"
+report_name <- "BatterRoster.txt"
+file_path <- paste(report_path,report_name,sep = "")
+
+readDMBfile <- function(file_path) {
+  ## Read in file and separate header information from data
+  file <- readLines(file_path, warn = FALSE)
+  file <- file[5:length(file) - 1]
+  headers <- file[1]
+  data <- file[file != headers]
+  
+  ## Figure out how to split the rest of the lines
+  max_length <- nchar(headers)
+  field_name_matches <- gregexpr(" [A-Z]",headers)[[1]]
+  field_lengths <- append(field_name_matches[-1] - 1, max_length)
+  field_lengths <- field_lengths - append(0, field_lengths[-length(field_lengths)])
+  
+  ## Create temporary file for ease in reading back in as fixed-width
+  file_con <- file("temp_output.txt")
+  writeLines(append(headers,data), file_con)
+  close(file_con)
+  
+  ## Read in final data frame
+  output <- read.fwf("temp_output.txt", widths = field_lengths, stringsAsFactors = FALSE)
+  names(output) <- output[1,]
+  output <- output[-1,]
+  
+  ## Clean up unnecessary file
+  file.remove("temp_output.txt")
+  return(output)
+}
