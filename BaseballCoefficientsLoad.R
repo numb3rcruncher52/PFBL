eb58432a285e673688f331b721bdc2da0d23b5a5
Max Lyons <MaxL@IronHill.local>
2016-11-04 16:19:38 -0400

    Finalized Coefficient Loading

--- BaseballCoefficientsLoad.R
+++ BaseballCoefficientsLoad.R
@@ -16,12 +16,12 @@ coef_def <- read_csv("DATA/DefCoef.csv", col_types = "cciiiiiiiii")
 
 coef_OFarm <- read_csv("DATA/OFArm.csv", col_types = "ciii")
 
-#coef_Carm <- read_csv("DATA/CArm.csv", col_types = "")
-#names(coef_Carm) <- c("Arm","POS","RAA_throw")
+coef_Carm <- read_csv("DATA/CArm.csv", col_types = "cci")
+names(coef_Carm) <- c("Arm","POS","RAA_throw")
 
-#coef_oopDef <- read.csv("data/DMBOOPCoef.csv",header=T,stringsAsFactors = F)
+coef_oop_def <- read_csv("DATA/DMBOOPCoef.csv", col_types = "ccciiiiiiiii")
 
-## Get baserunning values in there as well
+coef_baserunning <- read_csv("DATA/RunCoef.csv", col_types = "ci")
 
 ## Pull seasonal constants from fangraphs
 fg_guts <- read_html("http://www.fangraphs.com/guts.aspx?type=cn")
@@ -37,12 +37,14 @@ coef_def <- coef_def %>%
   mutate(Err = as.numeric(Err))
 
 ## Clean up out of position coefficients
-#coef_oopDef2 <- coef_oopDef %>% gather(Err,RAA,'0':'200',convert = TRUE)
+coef_oop_def <- coef_oop_def %>% 
+  gather(Err,RAA,`0`:`200`,convert = TRUE)
 
 ### Clean up the outfield arm coefficients
 coef_arm <- coef_OFarm %>% 
-  gather(POS,RAA_throw,-Arm) 
-  #rbind(coef_Carm)
+  gather(POS,RAA_throw,-Arm) %>%
+  rbind(coef_Carm)
+rm(coef_OFarm, coef_Carm)
 
 coef_wOBA <- seasonal_constants %>%
   select(Season:wHR) %>%
