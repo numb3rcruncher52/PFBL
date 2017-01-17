1c528f3ec9d240a8b9eeb0058e4e9ba50caa9854
Max Lyons <MaxL@IronHill.local>
2017-01-16 21:58:23 -0500

    player stat calc using given season instead of creating value itself

--- Calculate_Player_Stats.R
+++ Calculate_Player_Stats.R
@@ -9,13 +9,12 @@
 calcPlayerStats <- function(stats) {
   
   stats %>%
-    mutate(Season = SEASON) %>%
-    left_join(seasonal_constants) %>%
+    left_join(seasonal_constants, by = c('season'='Season')) %>%
     mutate(PA = calcPA(AB, UBB, HBP, SF),
            wOBA = calcWOBA(PA, SNG, DBL, TRI, HR, UBB, HBP, 
                            wBB, wHBP, w1B, w2B, w3B, wHR),
            wRAA = calcWRAA(role, wOBA, lg_wOBA, wOBAScale, 1)) %>%
-    select(ID, Name, role, Season, split, OPS, PA, wOBA, wRAA) %>%
+    select(ID, Name, role, season, split, OPS, PA, wOBA, wRAA) %>%
     gather(stat_name,stat_value,OPS:wRAA) %>%
     mutate(stat_name = paste(split,"_",stat_name,sep="")) %>%
     select(-split) %>%
