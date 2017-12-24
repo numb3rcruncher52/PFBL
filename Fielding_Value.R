d6731eff2ff2f3b5beea6962aadde5d977bd3b9e
Max Lyons <MaxL@IronHill.local>
2017-12-24 10:34:10 -0500

    Use def model for only rated positions

--- Fielding_Value.R
+++ Fielding_Value.R
@@ -24,48 +24,57 @@ fieldingValue <- function(batter_ratings) {
            Arm = ifelse(POS=='C',C,ifelse(POS %in% c('LF','CF','RF'),OF,NA))) %>%
     select(-Range, -OF, -C)
   
-    ## Use linear defensive model to predict defensive value
-    raa_values <- predict(def_model, newdata = player_pos_ratings)
-    fielding <- player_pos_ratings %>%
+  ## Use linear defensive model to predict defensive value
+  raa_values <- predict(def_model, newdata = player_pos_ratings)
+  
+  fielding <- player_pos_ratings %>%
     mutate(RAA = raa_values) %>%
-    left_join(coef_arm)
-    
-    ## Find the upper and lower rating benchmarks for all rated
-    ## defensive positions
-    # mutate(Err_low = pmin(plyr::round_any(Err, 25, f = floor),175),
-    #        Err_high = pmin(plyr::round_any(Err, 25, f = ceiling),200)) 
-  
-  ## Join rated position information with out of position coefficients
-  fielding_unrated <- fielding %>%
-    left_join(coef_oop_def, 
-              by = c("POS"="OPOS", "RANGE"="RANGE")) %>%
-    left_join(coef_oop_def, 
-              by = c("POS"="OPOS", "RANGE"="RANGE", "Err_high"="Err")) %>%
-    mutate(POS = NPOS.x, rated = "unrated") %>%
-    select(-NPOS.x, -NPOS.y)
-  
-  ### Join rated position information with in-position coefficients
-  fielding_rated <- fielding %>%
-    left_join(coef_def, 
-              by = c("POS"="POS", "RANGE"="RANGE", "Err_low"="Err")) %>%
-    left_join(coef_def, 
-              by = c("POS"="POS", "RANGE"="RANGE", "Err_high"="Err")) %>%
-    mutate(rated = "rated")
-  
-  ## Combine both rated and unrated raw coefficients to calculate
-  ## defensive value, and keep the player-position combos that are
-  ## either rated or have the highest defensive value
-  fielding_value <- fielding_rated %>%
-    bind_rows(fielding_unrated) %>%
-    rename(RAA_low = RAA.x, RAA_high = RAA.y)  %>%
+    left_join(coef_arm) %>%
     mutate(RAA_throw = ifelse(is.na(RAA_throw),0,RAA_throw),
-           RAA = RAA_low + (Err - Err_low)*(RAA_high - RAA_low)/25 + 
+           RAA = RAA + 
              RAA_throw,
-           RAA_PA = RAA / TOTAL_PA_FULL) %>%
+           RAA_PA = RAA / TOTAL_PA_FULL
+           , rated = "YES") %>%
     select(ID, Name, season, POS, rated, RANGE, Err, Arm, RAA, RAA_PA) %>%
     arrange(ID, Name, season, POS, rated, desc(RAA)) %>%
     distinct(ID, Name, season, POS, .keep_all = TRUE) %>%
     filter(!is.na(POS))
   
-  return(fielding_value)
+  ## Find the upper and lower rating benchmarks for all rated
+  ## defensive positions
+  # mutate(Err_low = pmin(plyr::round_any(Err, 25, f = floor),175),
+  #        Err_high = pmin(plyr::round_any(Err, 25, f = ceiling),200)) 
+  
+  ## Join rated position information with out of position coefficients
+  # fielding_unrated <- fielding %>%
+  #   
+  #   left_join(coef_oop_def, 
+  #             by = c("POS"="OPOS", "RANGE"="RANGE", "Err_high"="Err")) %>%
+  #   mutate(POS = NPOS.x, rated = "unrated") %>%
+  #   select(-NPOS.x, -NPOS.y)
+  # 
+  # ### Join rated position information with in-position coefficients
+  # fielding_rated <- fielding %>%
+  #   left_join(coef_def, 
+  #             by = c("POS"="POS", "RANGE"="RANGE", "Err_low"="Err")) %>%
+  #   left_join(coef_def, 
+  #             by = c("POS"="POS", "RANGE"="RANGE", "Err_high"="Err")) %>%
+  #   mutate(rated = "rated")
+  
+  ## Combine both rated and unrated raw coefficients to calculate
+  ## defensive value, and keep the player-position combos that are
+  ## either rated or have the highest defensive value
+  # fielding_value <- fielding_rated %>%
+  #   bind_rows(fielding_unrated) %>%
+  #   rename(RAA_low = RAA.x, RAA_high = RAA.y)  %>%
+  #   mutate(RAA_throw = ifelse(is.na(RAA_throw),0,RAA_throw),
+  #          RAA = RAA_low + (Err - Err_low)*(RAA_high - RAA_low)/25 + 
+  #            RAA_throw,
+  #          RAA_PA = RAA / TOTAL_PA_FULL) %>%
+  #   select(ID, Name, season, POS, rated, RANGE, Err, Arm, RAA, RAA_PA) %>%
+  #   arrange(ID, Name, season, POS, rated, desc(RAA)) %>%
+  #   distinct(ID, Name, season, POS, .keep_all = TRUE) %>%
+  #   filter(!is.na(POS))
+  
+  return(fielding)
 }
