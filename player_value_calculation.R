77ae0a06466953d9510d673bca64adad0f64e115
Max Yoga <mwlyons52@gmail.com>
2018-01-04 13:59:28 -0500

    refactor by using LATEST_SEASON instead of manually adding seasons

--- player_value_calculation.R
+++ player_value_calculation.R
@@ -9,6 +9,7 @@ TOTAL_PA_FULL <- LH_PA_FULL + RH_PA_FULL
 PA_INN <- 4.277
 INN_START_MAX <- 7
 PITCH_LH_SPLIT <- 0.4357984
+LATEST_SEASON <- 2017
 
 REPORT_DIR <- "C:\\Users\\mwlyo\\Dropbox\\PFBL\\Reports - DMB\\"
 REPORT_DIR <- "C:\\Users\\maxl\\Dropbox (Personal)\\PFBL\\Reports - DMB\\"
@@ -16,15 +17,15 @@ REPORT_DIR <- "C:\\Users\\maxl\\Dropbox (Personal)\\PFBL\\Reports - DMB\\"
 # Source Helper functions -------------------------------------------------
 
 source("DMBreportLoad.R")
-source("BaseballCoefficientsLoad.R")
 source("Fielding_Value.R")
 source("Calculate_Player_Stats.R")
 source("functions.R")
+source("BaseballCoefficientsLoad.R")
 
-# Load data for season in question ----------------------------------------
+# Load data for all available seasons ----------------------------------------
 
-season_folders <- c("reports_2015", "reports_2016", "reports_2017", "reports_2018")
-seasons <- seq(2014, 2017, 1)
+seasons <- seq(2014, LATEST_SEASON, 1)
+season_folders <- paste("reports", seasons + 1, sep = "_")
 
 args2 <- list(directory = paste0(REPORT_DIR,season_folders,"\\"),
               season = seasons)
