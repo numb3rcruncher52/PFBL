f0d7b2f2c8046578fe501a6d38e4b68022c71a2f
Max Yoga <mwlyons52@gmail.com>
2019-01-22 22:49:23 -0500

    Updated function to get latest rosters

--- Load_Rosters.R
+++ Load_Rosters.R
@@ -3,11 +3,13 @@
 #
 # Given a directory, load the most current roster information
 ##############################################################
+library(tidyverse)
+source("DMBreportLoad.R")
 
-SEASON <- 2016
+SEASON <- 2018
 
-roster_directory <- "C:\\dmb11\\PFBL 2017\\reports\\"
+roster_directory <- "C:\\dmb11\\PFBL 2019\\reports\\"
 
 rosters <- readRosterStatus(roster_directory, SEASON)
 
-write_csv(rosters, "Output/rosters_current.csv")
+write_csv(rosters, "rosters_current.csv")
