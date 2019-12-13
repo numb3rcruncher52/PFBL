ad6780e95c44281c99ffdf52623d75f8d31a05ea
Max Lyons <max.lyons@MLyons-MBP.local>
2019-12-13 07:33:52 -0600

    Add league information to roster files

--- Solution Scripts/UpdateRosters.R
+++ Solution Scripts/UpdateRosters.R
@@ -8,6 +8,7 @@ rm(list=ls())
 
 LATEST_SEASON <- 2020
 REPORT_DIR <- paste0("~/OneDrive/PFBL/Reports - DMB/reports_",LATEST_SEASON,"/")
+MAPPINGS_DIR <- "~/OneDrive/PFBL/Reports - DMB/MAPPINGS/"
 
 timeframe_list <- c("01_New Season"
                     , "02_Pre Draft"
@@ -21,6 +22,12 @@ library(tidyverse)
 
 source("./DMBreportLoad.R")
 
-rosters <- readRosterStatus(REPORT_DIR, LATEST_SEASON)
+dim_team <- read_csv(paste0(MAPPINGS_DIR, "dimTeam.csv"), col_types = "ccicc")
+
+rosters <- readRosterStatus(REPORT_DIR, LATEST_SEASON) %>%
+  select(ID, Name, Team, season) %>%
+  left_join(dim_team, by = c('season' = 'Season', 'Team' = 'Team'))
+
+head(rosters)
 
 write_csv(rosters, paste0("OUTPUT_NEW/rosters_",LATEST_SEASON,"_",CURRENT_TIMEFRAME,".csv"))
